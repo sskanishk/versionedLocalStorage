f72574162213648606fad674c13e4596adb8684e
sskanishk <kanishkmalviya78@gmail.com>
2024-09-06 09:18:09 +0530

    feat: added rollback based on timestamp

--- versionedLocalStorage.js
+++ versionedLocalStorage.js
@@ -14,16 +14,25 @@ class VersionedLocalStorage {
 
 
     // Save a new version of the key
-    saveVersion(key, value) {
+    _saveVersion(key, value) {
         const versions = JSON.parse(localStorage.getItem(this.versionKey)) || {};
         if (!versions[key]) {
             versions[key] = [];
         }
-
         const timestamp = new Date().toISOString();
         versions[key].push({ timestamp, value });
-
         localStorage.setItem(this.versionKey, JSON.stringify(versions));
+    }
+
+    // Parse value in json only if it is string 
+    _parseIfString(value) {
+        let checkValue = typeof value === 'string' ? JSON.parse(value) : value;
+        console.log("checkValue", checkValue)
+        if(typeof value === 'string') {
+            return JSON.parse(value)
+        } else {
+            return value
+        }
     }
 
     // Set a new key with versioning
@@ -34,16 +43,14 @@ class VersionedLocalStorage {
         };
 
         localStorage.setItem(key, JSON.stringify(versionedValue));
-        this.saveVersion(key, versionedValue);
+        this._saveVersion(key, versionedValue);
     }
 
     // Get the latest version of the key
     get(key) {
         const item = localStorage.getItem(key);
         if (!item) return null;
-
         const { value } = JSON.parse(item);
-
         return JSON.parse(value);
     }
 
@@ -56,20 +63,35 @@ class VersionedLocalStorage {
     }
 
     // Get the version history of a particular key
-    getVersionHistory(key) {
+    _getVersionHistory(key) {
         const versions = JSON.parse(localStorage.getItem(this.versionKey)) || {};
         return versions[key] || [];
     }
 
     // Get the all versions history of a particular key
     getAllVersions(key) {
-        const history = this.getVersionHistory(key)
+        const history = this._getVersionHistory(key)
         if (history.length > 0) {
             return history.map(version => ({
-                value: typeof version.value === 'string' ? JSON.parse(version.value) : version.value,
+                value: this._parseIfString(version.value),
                 timestamp: version.timestamp
             }));
         }
         return [];
+    }
+
+    // Rollback to a specific version based on timestamp
+    rollback(key, timestamp) {
+        const history = this._getVersionHistory(key);
+        // console.log("history ", history);
+        const version = history.find(v => v.timestamp === timestamp);
+        // console.log("version ", version);
+        if (version) {
+            const value = this._parseIfString(version.value);
+            this.set(key, value);
+            return value;
+        } else {
+            throw new Error('Version not found.');
+        }
     }
 }
