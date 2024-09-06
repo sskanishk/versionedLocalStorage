509cb9778cbcc2745014d37f65c06295c8897bf7
sskanishk <kanishkmalviya78@gmail.com>
2024-09-06 23:24:58 +0530

    feat: added ttl for local storage key

--- versionedLocalStorage.js
+++ versionedLocalStorage.js
@@ -4,6 +4,7 @@ class VersionedLocalStorage {
         this.versionKey = `${namespace}_versions`;
         this.init();
     }
+    
 
     // Initialize the version storage if not present
     init() {
@@ -24,6 +25,7 @@ class VersionedLocalStorage {
         localStorage.setItem(this.versionKey, JSON.stringify(versions));
     }
 
+
     // Parse value in json only if it is string 
     _parseIfString(value) {
         let checkValue = typeof value === 'string' ? JSON.parse(value) : value;
@@ -35,32 +37,54 @@ class VersionedLocalStorage {
         }
     }
 
-    // Set a new key with versioning
-    set(key, value) {
+
+    _isKeyExpired(key, storedItem) {
+        if (storedItem.ttl && Date.now() > storedItem.ttl) {
+            // console.log("here")
+            this.del(key);  // Remove the expired key
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    // Set a new key with versioning and TTL
+    set(key, value, ttl=null) {
         const versionedValue = {
             value: JSON.stringify(value), 
             timestamp: new Date().toISOString(),
+            ttl: ttl ? Date.now() + ttl * 1000 : null
         };
-
         localStorage.setItem(key, JSON.stringify(versionedValue));
         this._saveVersion(key, versionedValue);
     }
 
+
     // Get the latest version of the key
     get(key) {
-        const item = localStorage.getItem(key);
-        if (!item) return null;
-        const { value } = JSON.parse(item);
+        const storedItemJSON = localStorage.getItem(key);
+        if (!storedItemJSON) return null;
+        const storedItem = JSON.parse(storedItemJSON) || {};
+        if (this._isKeyExpired(key, storedItem)) return null;
+        const { value } = storedItem;
         return JSON.parse(value);
     }
+
 
     // Delete a key
     del(key) {
         localStorage.removeItem(key);
         const versions = JSON.parse(localStorage.getItem(this.versionKey)) || {};
-        delete versions[key];
-        localStorage.setItem(this.versionKey, JSON.stringify(versions));
+        if(versions[key]) {
+            delete versions[key];
+            localStorage.setItem(this.versionKey, JSON.stringify(versions));
+            return true;
+        } else {
+            return false;
+        }
     }
+
 
     // Get the version history of a particular key
     _getVersionHistory(key) {
@@ -68,17 +92,20 @@ class VersionedLocalStorage {
         return versions[key] || [];
     }
 
+
     // Get the all versions history of a particular key
     getAllVersions(key) {
         const history = this._getVersionHistory(key)
         if (history.length > 0) {
             return history.map(version => ({
                 value: this._parseIfString(version.value),
-                timestamp: version.timestamp
+                timestamp: version.timestamp,
+                ttl: version.ttl,
             }));
         }
         return [];
     }
+
 
     // Rollback to a specific version based on timestamp
     rollback(key, timestamp) {
